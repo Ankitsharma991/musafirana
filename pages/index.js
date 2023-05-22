8aeea6bbaed18dd7125d4aa3094692b30e2c1b52
Ankit Sharma <sharmaa7861@gmail.com>
2023-05-22 19:17:10 +0530

    updating changes[slidebar]

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.icon" />
-      </Head>
-      <h1>This is Musafirana (a spotify 2.0)</h1>
+    <div className="bg-black h-screen overflow-hidden">
+      <main>
+        <Sidebar />
+      </main>
+      <div></div>
     </div>
   );
 }
