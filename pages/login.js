a57c7c5759231684010b7fbe97fb219e0bcd6c35
Ankit Sharma <sharmaa7861@gmail.com>
2023-05-23 19:56:35 +0530

    login with providers

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { getProviders, signIn } from "next-auth/react";
 
-function login() {
-  return <div>login</div>;
+function Login({ providers }) {
+  console.log("Providers : ", providers);
+  return (
+    <div>
+      <img
+        className="w-52 mb-5"
+        src="https://img.icons8.com/?size=512&id=iefkXAGbJmaP&format=png"
+        alt="picture"
+      />
+      {/* {Object.values(providers).map((provider) => (
+        <div>
+          <button>Login with {provider.name}</button>
+        </div>
+      ))} */}
+    </div>
+  );
 }
 
-export default login;
+export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
