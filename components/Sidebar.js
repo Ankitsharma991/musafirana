9465c79f4fd5337e12641f699140f221c804d786
Ankit Sharma <sharmaa7861@gmail.com>
2023-05-25 11:00:30 +0530

    Playlists fetched from spotify

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,3 +1,4 @@
+import useSpotify from "@/hooks/useSpotify";
 import {
   HomeIcon,
   SearchIcon,
@@ -7,9 +8,21 @@ import {
   RssIcon,
 } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 function Sidebar() {
+  const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
+
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen ">
       <div className="space-y-4">
@@ -46,16 +59,14 @@ function Sidebar() {
           <p>Your episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+
+        {playlists.map((playlist) => {
+          return (
+            <p key={playlist.id} className="cursor-pointer hover:text-white">
+              {playlist.name}
+            </p>
+          );
+        })}
       </div>
     </div>
   );
